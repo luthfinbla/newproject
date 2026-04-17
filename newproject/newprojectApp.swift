ce1840f77fbb0db6ff30ff0f43da039b11671ee9
luthfi <luthfinabila240@gmail.com>
2026-04-17 22:12:24 +0700

    Initial Commit

---
+++ newproject/newprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  newprojectApp.swift
+//  newproject
+//
+//  Created by Mac on 17/04/26.
+//
+
+import SwiftUI
+
+@main
+struct newprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
